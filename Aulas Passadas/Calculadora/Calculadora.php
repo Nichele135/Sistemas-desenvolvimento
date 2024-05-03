ad9fdfefeb39583eec41a814b5a8bb3bd2f09ea3
Andre Nichele <andrenic135@gmail.com>
2024-05-03 11:01:55 -0300

    Calculadora novo

--- Aulas Passadas/Calculadora/Calculadora.php
+++ Aulas Passadas/Calculadora/Calculadora.php
@@ -32,44 +32,75 @@
     <input name="num2" type="text" id="num2" placeholder="Numero2">
 
     <button type="submit">Calcular</button>
+    <button type="submit" name="historico">Histórico</button>
 
     <?php
-        
-        
+
+        $historico = '';
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['historico'])) {
+         // Recupera os números e o operador
         $num1 = $_POST['num1'];
+        $historicoN1 = $_POST['num1'];
         $num2 = $_POST['num2'];
-        
-        function conta($num1, $num2){
-
+        $historicoN2 = $_POST['num2'];
         $operador = $_POST['operador'];
-
-            switch ($operador) {
-                case '+':
-                    echo $num1 + $num2;
-                    break;
+        $historicoOP = $_POST['operador'];
+        
+        
+        switch ($operador) {
+            case '+':
+                $resultado = $num1 + $num2;
+                echo $resultado;
+                break;
                 case '-':
-                    echo $num1 - $num2;
+                    $resultado = $num1 - $num2;
+                    echo $resultado;
                     break;
-                case '*':
-                    echo $num1 * $num2;
+                    case '*':
+                        $resultado = $num1 * $num2;
+                        echo $resultado;
+                        break;
+                        case '/':
+                            if ($num2 == 0) {
+                                echo "ERRO, divisão por zero!!!";
+                            } else {
+                                $resultado = $num1 / $num2;
+                                echo $resultado;
+                            }
+                            break;
+                            case '^':
+                                //pesquisei pelo chat esse pow
+                                $resultado = pow($num1, $num2);
+                                echo $resultado;
+                                break;
+                                case '!':
+                    echo factorial($num1);
+                    echo "  (Caso tentou preencher a segunda caixa, só ira aparecer o factorial do primeiro número informado)";
                     break;
-                case '/':
-                    echo $num1 / $num2;
+                    
+                    default:
+                    echo "Informe um numero válido";
                     break;
-                case '^':
-                    echo $num1 ^ $num2;
-                    break;
-                case '!':
-                    echo $num1 ! $num2;
-                    break;
-                
-                default:
-                    echo "Digite novamente";
-                    break;
+                }
+                $historico = "$num1 $operador $num2 = $resultado";
             }
-
+            
+            if (isset($_POST['historico'])) {
+            echo "<h2>Histórico:</h2>";
+            echo "$historicoN1 $historicoOP $historicoN2";
+        } elseif (isset($_POST['historico']) && empty($historico)) {
+            echo "<h2>Histórico:</h2>";
+            echo "Nenhum histórico disponível.";
         }
-        conta($num1, $num2);
+
+
+        function factorial($num){
+            if ($num == 0) {
+                return 1;
+            }
+            return $num * factorial($num - 1);
+        }
 
 
     ?>
