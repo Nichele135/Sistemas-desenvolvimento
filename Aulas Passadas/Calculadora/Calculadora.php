74f63e410a2d35773cf8b5bb5f107cac4115c69a
Andre Nichele <andrenic135@gmail.com>
2024-05-04 23:25:36 -0300

    Atulizando calculadora semi pronta

--- Aulas Passadas/Calculadora/Calculadora.php
+++ Aulas Passadas/Calculadora/Calculadora.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,71 +11,122 @@
     </style>
 
 </head>
+
 <body>
     <h1>Calculadora PHP</h1>
 
-    <form action="" method="post">
+        <form action="" method="post">
 
-    <label for="num1"></label>
-    <input name="num1" type="text" id="num1" placeholder="Numero 1">
+            <label for="num1"></label>
+            <input name="num1" type="text" id="num1" placeholder="Numero 1">
 
-    <select name="operador" id="operador">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-        <option value="^">^</option>
-        <option value="!">!</option>
-    </select>
-
-
-    <label for="num2"></label>
-    <input name="num2" type="text" id="num2" placeholder="Numero2">
-
-    <button type="submit">Calcular</button>
-
-    <?php
+            <select name="operador" id="operador">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+                <option value="^">^</option>
+                <option value="!">!</option>
+            </select>
 
 
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
+        <label for="num2"></label>
+        <input name="num2" type="text" id="num2" placeholder="Numero2">
 
-        function conta($num1, $num2){
+        <button type="submit">Calcular</button>
+        <button type="submit" name="historico">Histórico</button>
+        <input type="submit" name="limpar" value="Limpar Histórico">
 
-        $operador = $_POST['operador'];
+        </form>
+
+<?php
+
+    session_start();
+
+        // Serve para mostrar o historico
+        if (isset($_POST['historico'])) {
+            if (!empty($_SESSION['historico'])) {
+                echo "<h2>Histórico:</h2>";
+                foreach ($_SESSION['historico'] as $op) {
+                    echo "$op<br>";
+                }
+            }
+        }
+        //serve para limpar o historico
+
+        if (isset($_POST['limpar'])) {
+            unset($_SESSION['historico']);
+        }
+
+        //if para mudar os numeros para inteiros, pois sempre dava erro no resultado, pesquisei muito mas so consegui achar essa solucao
+
+        if(isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operador'])) {
+            $num1 = intval($_POST['num1']);
+            $num2 = intval($_POST['num2']);
+            $operador = $_POST['operador'];
+
+
+        //Iniciando a function
+            conta($operador, $num1, $num2);
+        }
+        
+        //aqui criei uma function para fazer as contas pedidas
+        function conta($operador, $num1, $num2){
+
+
 
             switch ($operador) {
                 case '+':
-                    echo $num1 + $num2;
+                    $resultado = $num1 + $num2;
                     break;
                 case '-':
-                    echo $num1 - $num2;
+                    $resultado = $num1 - $num2;
                     break;
                 case '*':
-                    echo $num1 * $num2;
+                    $resultado = $num1 * $num2;
                     break;
                 case '/':
-                    echo $num1 / $num2;
+                    $resultado = $num1 / $num2;
                     break;
                 case '^':
-                    echo $num1 ^ $num2;
+                    $resultado = pow($num1, $num2);
                     break;
                 case '!':
-                    echo $num1 ! $num2;
+                    $resultado = calcularFatorial($num1);
                     break;
-
                 default:
-                    echo "Digite novamente";
+                    $resultado = "Digite novamente";
                     break;
             }
 
+
+            if($num1 == 0 || $num2 == 0 ){
+            } else {
+            $_SESSION['historico'][] = "$num1 $operador $num2 = $resultado";
+        }   
+        
+        echo $resultado;
+}
+
+
+        //function para calcular o fatorial achei mais facil fazer assim do que deixar uma bagunça la dentro do switch
+        function calcularFatorial($num){
+            if ($num == 0) {
+                return 1;
+            } else {
+                return $num * calcularFatorial($num - 1);
+            }
         }
-        conta($num1, $num2);
 
 
-    ?>
 
-</form>
+
+?>
+
+
+
+    
 
 </body>
+
 </html>
